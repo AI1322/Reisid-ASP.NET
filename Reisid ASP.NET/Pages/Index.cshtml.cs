5867e69d81aca0390f5a3b453108bb9204c753d1
opilane <opilane@HP-CZC2349HTJ>
2025-09-26 14:28:58 +0300

    Add project files.

---
+++ Reisid ASP.NET/Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Hosting.Internal;
+using System.Xml.Xsl;
+
+namespace Reisid_ASP.NET.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string TransformedXml { get; private set; }
+
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public IndexModel(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public void OnGet()
+        {
+            try
+            {
+                var xmlPath = Path.Combine(_hostingEnvironment.ContentRootPath, "wwwroot", "Reisid.xml");
+                var xsltPath = Path.Combine(_hostingEnvironment.ContentRootPath, "wwwroot", "Reisid.xslt");
+
+                if (!System.IO.File.Exists(xmlPath) || !System.IO.File.Exists(xsltPath))
+                {
+                    TransformedXml = "<p class='text-danger'>Error: XML or XSLT file not found.</p>";
+                    return;
+                }
+
+                var xslt = new XslCompiledTransform();
+                xslt.Load(xsltPath);
+
+                using (var sw = new StringWriter())
+                {
+                    xslt.Transform(xmlPath, null, sw);
+                    TransformedXml = sw.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                TransformedXml = $"<p class='text-danger'>An error occurred during XML transformation: {ex.Message}</p>";
+            }
+        }
+    }
+}
---
+++ Reisid ASP.NET/wwwroot/Reisid.xslt
@@ -0,0 +1,64 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+	<xsl:output method="html" indent="yes"/>
+
+	<xsl:template match="/">
+		<html>
+			<head>
+				<style>
+					table, th, td {
+					border: 1px solid black;
+					border-collapse: collapse;
+					padding: 3px;
+					}
+				</style>
+			</head>
+			<body>
+				<table>
+					<tr>
+						<th>ID</th>
+						<th>Reisi number</th>
+						<th>Lennufirma</th>
+						<th>Sihtkoht</th>
+						<th>Algus aeg</th>
+						<th>Lõppu aeg</th>
+						<th>Valjumine</th>
+						<th>Saabumine</th>
+						<th>Hind</th>
+					</tr>
+					<xsl:for-each select="/Reisid/Reis">
+						<tr>
+							<td>
+								<xsl:value-of select="@id"/>
+							</td>
+							<td>
+								<xsl:value-of select="@reisi_number"/>
+							</td>
+							<td>
+								<xsl:value-of select="Lennufirma"/>
+							</td>
+							<td>
+								<xsl:value-of select="Sihtkoht"/>
+							</td>
+							<td>
+								<xsl:value-of select="Lennujaamad/@algus_aeg"/>
+							</td>
+							<td>
+								<xsl:value-of select="Lennujaamad/@loppu_aev"/>
+							</td>
+							<td>
+								<xsl:value-of select="Lennujaamad/Valjumine"/>
+							</td>
+							<td>
+								<xsl:value-of select="Lennujaamad/Saabumine"/>
+							</td>
+							<td>
+								<xsl:value-of select="Hind/@value"/>
+							</td>
+						</tr>
+					</xsl:for-each>
+				</table>
+			</body>
+		</html>
+	</xsl:template>
+</xsl:stylesheet>
